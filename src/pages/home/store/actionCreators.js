92e50c4f5edce354ad6f560a86b4e748542afb1e
Liepin-NorthAmerica <nini10171017>
2019-10-23 06:30:40 -0400

    prod branch

--- src/pages/home/store/actionCreators.js
+++ src/pages/home/store/actionCreators.js
@@ -6,6 +6,7 @@ import { selectedCols } from '../../../utils/utils';
 import { COLUMN_SCHEMA } from '../../../utils/constants';
 
 
+
 const initListAction = (data, startIdx, endIdx) => ({
     type: constants.INIT_LIST_ACTION,
     data: fromJS(data),
@@ -15,9 +16,11 @@ const initListAction = (data, startIdx, endIdx) => ({
 
 export const getList = (startIdx, endIdx) => {
     return async (dispatch) => {
-        const response = await axios.get(`${uri}transactions`)
+        // const response = await axios.get(`${uri}transactions`)
+        const response = await axios.get('data.json')
               .catch(err => console.log(err));
-        const alldata = selectedCols(response.data, COLUMN_SCHEMA.NAMES);
+        // const alldata = selectedCols(response.data, COLUMN_SCHEMA.NAMES);
+        const alldata = selectedCols(response.data.transactions, COLUMN_SCHEMA.NAMES);
         const action = initListAction(alldata,startIdx,endIdx);
         dispatch(action);
     }
